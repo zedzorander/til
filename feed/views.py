18e1de570317dc38ba24154c9754539d0e0a56d9
zedzorander <colephares@gmail.com>
2021-07-14 16:14:03 -0700

    added some jquery

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 from typing import List
 
 from .models import Post
@@ -34,3 +35,19 @@ class CreateNewPost(LoginRequiredMixin, CreateView):
         obj.author = self.request.user
         obj.save()
         return super().form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+        post = Post.objects.create(
+            text = request.POST.get("text"),
+            author = request.user
+        )
+        
+        return render(
+            request,
+            "includes/post.html",
+            {
+                "post": post,
+                "show_detail_link": True,
+            },
+            content_type = "application"
+        )
