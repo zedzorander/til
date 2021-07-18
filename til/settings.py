b45968da2732e859ab55b98898520b1b4fe24c54
zedzorander <colephares@gmail.com>
2021-07-17 17:47:09 -0700

    overriding allauth login file

--- til/settings.py
+++ til/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(PROJECT_DIR, "til/templates")
+            os.path.join(PROJECT_DIR, "til/templates"),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
